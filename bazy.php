ff7f6af21c971ac76b1c1eab4160fb2b7f505cfe
Tomasz <Tomasz.gibas104@gmail.com>
2025-10-16 10:33:31 +0200

    16.10

--- bazy.php
+++ bazy.php
@@ -123,6 +123,19 @@ if (mysqli_num_rows($result) > 0){
     echo "0 results";
 }
 
+// wypisywanie 2 w liscie
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0){
+    echo "<ul>";
+    while($row = mysqli_fetch_row($result)) {
+        echo "<li>" . $row[0] . "" . $row[1] . "" . $row[2] . "</li>";
+    }
+     echo "</ul>";
+} else {
+    echo "0 results";
+}
 
 
 
